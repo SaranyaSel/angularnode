522dce31f51a6deb5225b610cd2f214a4bd0f69f
SaranyaSel <saranya.professional7@gmail.com>
2021-10-20 08:40:04 +1100

    updated the modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-@NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+import { CreatenodeComponent } from './createnode/createnode.component';
+import { DeletenodeComponent } from './deletenode/deletenode.component';
+import { DialogComponent } from './dialog/dialog.component';
+
+
+@NgModule({  
+  entryComponents: [
+    DialogComponent
+  ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTreeModule,
+    MatSelectModule,
+    BrowserAnimationsModule 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent,
+    CreatenodeComponent,
+    DeletenodeComponent,
+    DialogComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
